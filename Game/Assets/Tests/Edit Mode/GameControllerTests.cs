a5cae645998ebac09f2cba3d25b645a191bd660a
AleksanderMisztal <57528096+AleksanderMisztal@users.noreply.github.com>
2020-08-18 11:43:07 +0200

    Set up simple tests using unity framework

--- Game/Assets/Tests/Edit Mode/GameControllerTests.cs
+++ Game/Assets/Tests/Edit Mode/GameControllerTests.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
+using Assets.Scripts.UnityStuff;
 using NUnit.Framework;
-using UnityEngine.TestTools;
+using Planes262.GameLogic;
+using Planes262.Utils;
 
-namespace Tests
+namespace GameTests
 {
     public class GameControllerTests
     {
-        [Test]
-        public void GameControllerTestsSimplePasses()
-        {
 
-        }
-        [UnityTest]
-        public IEnumerator GameControllerTestsWithEnumeratorPasses()
+        private void Click(int x, int y)
         {
-            yield return null;
+            var cell = new VectorTwo(x, y);
+            MapController.OnCellClicked(cell);
+        }
+
+        [Test]
+        public void Should_BehaveWell_When_DoubleFight()
+        {
+            EventHandlers.OnGameJoined("Bot", PlayerSide.Blue, new Board(8, 5));
+            List<Troop> troops = new List<Troop>
+            {
+                Troop.Blue(2, 2, 2),
+                Troop.Blue(1, 3, 2),
+                Troop.Red(3, 2),
+                Troop.Red(4, 2),
+            };
+            EventHandlers.OnTroopsSpawned(troops);
+            Click(2, 2);
+            Click(3, 2);
+            Click(3, 2);
         }
     }
 }
---
+++ Game/Assets/Tests/Edit Mode/PathFinderTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Assets.Scripts.GameLogic;
+using Planes262.GameLogic;
+using Planes262.Utils;
+using UnityEngine;
+
+namespace Tests
+{
+    public class PathFinderTests
+    {
+        private TroopMap map;
+        private PathFinder pathFinder;
+
+        private void CreatePathFinder(List<Troop> troops)
+        {
+            map = new TroopMap();
+            map.SpawnWave(troops);
+            pathFinder = new PathFinder(map);
+        }
+
+        private void AssertSetEquality<T>(HashSet<T> actual, List<T> expected)
+        {
+            Debug.Log($"Counts: {actual.Count} : {expected.Count}");
+            foreach (var item in actual)
+                Debug.Log(item);
+            Assert.IsTrue(actual.Count == expected.Count);
+            foreach (var item in expected)
+                Assert.IsTrue(actual.Contains(item));
+        }
+
+
+        [Test]
+        public void Should_ReturnControllZone_When_TroopHasOneMove()
+        {
+            List<Troop> troops = new List<Troop>
+            {
+                Troop.Blue(2, 2, 1)
+            };
+            CreatePathFinder(troops);
+
+            var cells = pathFinder.GetReachableCells(troops[0].Position);
+
+            List<VectorTwo> expected = new List<VectorTwo>
+            {
+                new VectorTwo(2, 3),
+                new VectorTwo(3, 2),
+                new VectorTwo(2, 1),
+            };
+
+            AssertSetEquality(cells, expected);
+        }
+
+        [Test]
+        public void Should_NotReachCell_When_OccupiedByFriend()
+        {
+            List<Troop> troops = new List<Troop>
+            {
+                Troop.Blue(2, 2, 2),
+                Troop.Blue(2, 3)
+            };
+            CreatePathFinder(troops);
+
+            var cells = pathFinder.GetReachableCells(troops[0].Position);
+
+            List<VectorTwo> expected = new List<VectorTwo>
+            {
+                new VectorTwo(3, 2),
+                new VectorTwo(3, 3),
+                new VectorTwo(4, 2),
+                new VectorTwo(3, 1),
+                new VectorTwo(2, 1),
+                new VectorTwo(3, 0),
+                new VectorTwo(2, 0),
+            };
+            AssertSetEquality(cells, expected);
+        }
+    }
+}
