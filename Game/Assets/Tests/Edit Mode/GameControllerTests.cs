452e40c911c9ec3915173068b23cee043df4f2aa
AleksanderMisztal <57528096+AleksanderMisztal@users.noreply.github.com>
2020-08-18 11:19:30 +0200

    Create Unity tests

---
+++ Game/Assets/Tests/Edit Mode/GameControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GameControllerTests
+    {
+        [Test]
+        public void GameControllerTestsSimplePasses()
+        {
+
+        }
+        [UnityTest]
+        public IEnumerator GameControllerTestsWithEnumeratorPasses()
+        {
+            yield return null;
+        }
+    }
+}
