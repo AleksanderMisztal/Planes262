94b2e8c2fdc5e8ac049d578d044eebeb9e4049e4
AleksanderMisztal <57528096+AleksanderMisztal@users.noreply.github.com>
2020-10-21 16:05:08 +0100

    Backup

--- copyDLLs.bat
+++ copyDLLs.bat
@@ -2,6 +2,7 @@ cd "C:\Users\aleks\Projects\TS Games\Planes262\GameDataStructures"
 rd /s /q bin
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"  GameDataStructures.csproj /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
 xcopy /y "C:\Users\aleks\Projects\TS Games\Planes262\GameDataStructures\bin\Release\net471\GameDataStructures.dll" "C:\Users\aleks\Projects\TS Games\Planes262\Game\Assets"
+xcopy /y "C:\Users\aleks\Projects\TS Games\Planes262\GameDataStructures\bin\Release\net471\GameDataStructures.dll" "C:\Users\aleks\Projects\TS Games\Level Editor\Assets"
 
 cd ..\GameJudge
 rd /s /q bin
